59e9d7bde952245b3e718cb979c7f8b36c291f23
Benjamin Berglund <benjaminberglund@gmail.com>
2021-06-03 01:28:31 +0200

    added vue-jest testing and dependencies + wrote example tests

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <main> 
+    <h1>{{title}}</h1>
     <Checkout/>
   </main>
 </template>
 
 <script>
-import Checkout from '/src/components/Checkout.vue'
+import Checkout from '../src/components/Checkout.vue'
 
 export default{
+  data(){
+    return {
+      title: "Ye Olde Shoppe"
+    }
+  },
   components:{
     Checkout
   }
